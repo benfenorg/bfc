88f7e1539d83839632089244d0b090d0dddcd5e3
hellokittyboy-code <53204025+hellokittyboy-code@users.noreply.github.com>
2023-07-24 13:21:04 +0800

    Update setup.sh

--- .github/scripts/rosetta/setup.sh
+++ .github/scripts/rosetta/setup.sh
@@ -10,7 +10,7 @@ echo "run Sui genesis"
 sui genesis
 
 echo "generate rosetta configuration"
-sui-rosetta generate-rosetta-cli-config --online-url http://127.0.0.1:9002 --offline-url http://127.0.0.1:9003
+#sui-rosetta generate-rosetta-cli-config --online-url http://127.0.0.1:9002 --offline-url http://127.0.0.1:9003
 
 echo "install rosetta-cli"
 curl -sSfL https://raw.githubusercontent.com/coinbase/rosetta-cli/master/scripts/install.sh | sh -s
