7d832c00a7025ea58c0996011d8726b5af6bb76f
wanghui <grayking.w@gmail.com>
2023-09-28 16:29:23 +0800

    chore: add legacy sui-indexer as sui-indexer-v0

---
+++ crates/sui-indexer-v0/migrations/2023-09-12-001033_proposals/up.sql
@@ -0,0 +1,19 @@
+-- Your SQL goes here
+
+CREATE TABLE dao_proposals (
+  object_id       address   PRIMARY KEY,
+  action_id       BIGINT    NOT NULL,
+  action_name     TEXT      NOT NULL,
+  pid             BIGINT    NOT NULL,
+  proposer        address   NOT NULL,
+  start_time      BIGINT    NOT NULL,
+  end_time        BIGINT    NOT NULL,
+  for_votes       BIGINT    NOT NULL,
+  against_votes   BIGINT    NOT NULL,
+  eta             BIGINT    NOT NULL,
+  action_delay    BIGINT    NOT NULL,
+  quorum_votes    BIGINT    NOT NULL,
+  "state"         SMALLINT  NOT NULL
+);
+
+CREATE UNIQUE INDEX action_id_pid_uniq ON dao_proposals(action_id, pid);
