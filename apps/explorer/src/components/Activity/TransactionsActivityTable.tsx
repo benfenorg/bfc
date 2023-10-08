e32d4c2e91c53adf6acc610669119887ba865b35
lingqi <1062266294@qq.com>
2023-10-08 09:42:16 +0800

    list refresh

--- apps/explorer/src/components/Activity/TransactionsActivityTable.tsx
+++ apps/explorer/src/components/Activity/TransactionsActivityTable.tsx
@@ -26,7 +26,6 @@ interface Props {
 
 export function TransactionsActivityTable({
 	disablePagination,
-	refetchInterval,
 	initialLimit = DEFAULT_TRANSACTIONS_LIMIT,
 	transactionKindFilter,
 }: Props) {
@@ -42,7 +41,6 @@ export function TransactionsActivityTable({
 	const transactions = useGetTransactionBlocks(
 		transactionKindFilter ? { TransactionKind: transactionKindFilter } : undefined,
 		limit,
-		refetchInterval,
 	);
 	const { data, isFetching, pagination, isLoading, isError } = useCursorPagination(transactions);
 	const goToFirstPageRef = useRef(pagination.onFirst);
