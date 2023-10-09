fbd53d1cf091e2e94c776cedd369e539ce201e71
lingqi <1062266294@qq.com>
2023-10-09 15:36:18 +0800

    pnpm lock delete core

--- dapps/station/src/components/Base/Header.tsx
+++ dapps/station/src/components/Base/Header.tsx
@@ -8,21 +8,20 @@ import { ReactComponent as BenfenHeaderLogo } from '../../assets/benfen_header_l
 export function Header() {
 	const navigate = useNavigate();
 
-
 	return (
-		<div className="border-b border-gray-400">
-			<div>
+		<div className="">
+			<div className="flex px-10 py-5 items-center gap-7 border-b border-bf-hover">
 				<BenfenHeaderLogo />
+				<div className="font-semibold">BenFen Station</div>
 				{/* <button
 					className="text-lg font-bold text-center mr-3 bg-transparent ease-in-out duration-300 rounded border border-transparent py-2 px-4 bg-gray-200"
 					onClick={() => navigate('/')}
 				>
 					Station demo
 				</button> */}
-				
-				<div className="ml-auto my-3 md:my-1">
-					<SuiConnectButton></SuiConnectButton>
-				</div>
+			</div>
+			<div className="ml-auto my-3 md:my-1">
+				<SuiConnectButton></SuiConnectButton>
 			</div>
 		</div>
 	);
