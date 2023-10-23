8a951ef3ca8ea95d4b4422865859da3ea1af339f
wanghui <grayking.w@gmail.com>
2023-10-23 11:18:23 +0800

    chore: correct typoes in migrations

--- crates/sui-indexer-v0/migrations/2023-09-04-065252_24h/down.sql
+++ crates/sui-indexer-v0/migrations/2023-09-04-065252_24h/down.sql
@@ -3,6 +3,6 @@
 DROP TABLE IF EXISTS network_segment_metrics;
 DROP VIEW IF EXISTS network_overviews;
 DROP INDEX IF EXISTS addresses_last_appearance_time;
-ALTER TABLE checkpoints DROP COLUMN IF EXISTS total_transact_obc,
+ALTER TABLE checkpoints DROP COLUMN IF EXISTS total_transact_bfc,
       DROP COLUMN IF EXISTS system_tick;
-ALTER TABLE transactions DROP COLUMN IF EXISTS transact_obc;
+ALTER TABLE transactions DROP COLUMN IF EXISTS transact_bfc;
