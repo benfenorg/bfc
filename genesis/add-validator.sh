6690cf81c93e3115a7fb128079fbceeb4827b9d3
kakaxi <kakaxi002@hotmail.com>
2023-11-15 17:40:05 +0800

    add genesis tool bash.

---
+++ genesis/add-validator.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+read -p "Enter validator name:" name
+read -p "Enter validator ip:" ip
+read -p "Enter validator start port:" port
+
+num1=2
+port1=`expr $port`
+port2=`expr $port1 + $num1`
+port3=`expr $port2 + $num1`
+port4=`expr $port3 + $num1`
+
+
+
+bfc genesis-ceremony add-validator \
+--name "validator-$name" \
+--validator-key-file "validator-$name.key" \
+--worker-key-file "validator-$name-worker.key" \
+--account-key-file "validator-$name-account.key" \
+--network-key-file "validator-$name-network.key" \
+--network-address "/ip4/$ip/tcp/$port1/http" \
+--p2p-address "/ip4/$ip/udp/$port2" \
+--narwhal-primary-address "/ip4/$ip/udp/$port3" \
+--narwhal-worker-address "/ip4/$ip/udp/$port4" \
+--description abc \
+--image-url abc \
+--project-url abc
+
+echo "end of add validator"
--- genesis/generate-validator-key.sh
+++ genesis/generate-validator-key.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "start generating....input your validator name:"
 read -p "Enter your name:" name
-sui keytool generate-with-name bls12381 "validator-$name"
-sui keytool generate-with-name ed25519 "validator-$name-worker"
-sui keytool generate-with-name ed25519 "validator-$name-account"
-sui keytool generate-with-name ed25519 "validator-$name-network"
+bfc keytool generate-with-name bls12381 "validator-$name" 
+bfc keytool generate-with-name ed25519 "validator-$name-worker"
+bfc keytool generate-with-name ed25519 "validator-$name-account"
+bfc keytool generate-with-name ed25519 "validator-$name-network"
 
 echo "key done"
+
