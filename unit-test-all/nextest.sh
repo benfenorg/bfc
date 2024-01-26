f8c786817b44fb6dd7975ea875617ee05f235119
kakaxi <kakaxi002@hotmail.com>
2024-01-26 17:09:57 +0800

    update..

--- unit-test-all/nextest.sh
+++ unit-test-all/nextest.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd crates/sui-e2e-test
 #cargo nextest run --profile ci  --retries 1
 #https://nexte.st/book/filter-expressions.html
 cargo nextest run -E 'not test(/sim_.*/)' --profile ci  --retries 1
