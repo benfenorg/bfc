9213a13c78cfb67a81cf0dc073b2f687ec497637
suoyuan <suoyuan@gmail.com>
2024-03-07 11:10:32 +0800

    [swap]update swap test

--- crates/sui-e2e-tests/tests/reconfiguration_tests.rs
+++ crates/sui-e2e-tests/tests/reconfiguration_tests.rs
@@ -2365,7 +2365,7 @@ async fn swap_bfc_to_stablecoin_with_tag(test_cluster: &TestCluster, http_client
         SuiJsonValue::from_str(&SUI_CLOCK_OBJECT_ID.to_string())?,
         SuiJsonValue::new(json!("1000000000000"))?,
         SuiJsonValue::new(json!("0"))?,
-        SuiJsonValue::new(json!("1709622441776884"))?,
+        SuiJsonValue::new(json!("1719622441776"))?,
     ];
 
     let transaction_bytes: TransactionBlockBytes = http_client
