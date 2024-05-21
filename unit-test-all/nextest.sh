587663ca24e8d25728c6a16721086e7f6e8bcefb
kakaxi <kakaxi002@hotmail.com>
2024-05-21 16:20:10 +0800

    update..

--- unit-test-all/nextest.sh
+++ unit-test-all/nextest.sh
@@ -3,3 +3,10 @@
 #https://nexte.st/book/filter-expressions.html
 cargo nextest run -E 'not test(/sim_.*/)' --profile ci  --retries 1
 
+
+
+#
+#nohup cargo nextest run -E 'not test(/sim_.*/)' --profile ci  --retries 1  > cargo_logs 2>&1 &
+
+nohup cargo nextest run -E 'not test(/sim_.*/)' --profile ci  --retries 1  > cargo_logs 2>&1 && \
+nohup cargo simtest --profile ci  --retries 1 > simtest_logs 2>&1 &
