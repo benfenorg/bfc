51296c31b778f847feda1cef544025b204c7e5e2
kakaxi <kakaxi002@hotmail.com>
2024-06-04 16:09:56 +0800

    fix single-node-benchmark

--- crates/sui-single-node-benchmark/src/tx_generator/shared_object_create_tx_generator.rs
+++ crates/sui-single-node-benchmark/src/tx_generator/shared_object_create_tx_generator.rs
@@ -22,7 +22,7 @@ impl TxGenerator for SharedObjectCreateTxGenerator {
         TestTransactionBuilder::new(
             account.sender,
             account.gas_objects[0],
-            DEFAULT_VALIDATOR_GAS_PRICE,
+            DEFAULT_VALIDATOR_GAS_PRICE*6,
         )
         .move_call(
             self.move_package,
