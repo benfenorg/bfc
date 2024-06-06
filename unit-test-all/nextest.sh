d01b50c9ef2139aafd9ff742e1cca9ab38e48f37
kakaxi <kakaxi002@hotmail.com>
2024-06-06 11:38:39 +0800

    merge

--- unit-test-all/nextest.sh
+++ unit-test-all/nextest.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #cargo nextest run --profile ci  --retries 1
 #https://nexte.st/book/filter-expressions.html
+# --jobs 11 【limit working threads】
 cargo nextest run -E 'not test(/sim_.*/)' --profile ci  --retries 1
 
