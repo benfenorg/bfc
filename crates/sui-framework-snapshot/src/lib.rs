f24118d9dfdee4dfda5bd0696e0df2c3dd8215ca
wubin <18519247208@qq.com>
2024-06-28 10:45:27 +0800

    fix unit test

--- crates/sui-framework-snapshot/src/lib.rs
+++ crates/sui-framework-snapshot/src/lib.rs
@@ -7,7 +7,7 @@ use std::{fs, io::Read, path::PathBuf};
 use sui_framework::SystemPackage;
 use sui_types::base_types::ObjectID;
 use sui_types::{
-    DEEPBOOK_PACKAGE_ID, MOVE_STDLIB_PACKAGE_ID, SUI_FRAMEWORK_PACKAGE_ID, SUI_SYSTEM_PACKAGE_ID,
+    MOVE_STDLIB_PACKAGE_ID, SUI_FRAMEWORK_PACKAGE_ID, SUI_SYSTEM_PACKAGE_ID,
 };
 use sui_types::BFC_SYSTEM_PACKAGE_ID;
 
@@ -35,7 +35,6 @@ const SYSTEM_PACKAGE_PUBLISH_ORDER: &[ObjectID] = &[
     SUI_FRAMEWORK_PACKAGE_ID,
     BFC_SYSTEM_PACKAGE_ID,
     SUI_SYSTEM_PACKAGE_ID,
-    DEEPBOOK_PACKAGE_ID,
 ];
 
 pub fn load_bytecode_snapshot_manifest() -> SnapshotManifest {
