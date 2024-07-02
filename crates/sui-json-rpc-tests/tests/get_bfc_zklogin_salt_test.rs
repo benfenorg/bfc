0e06244f148ff3161aaebdfad0d24a166b394186
david <david20240515@gmail.com>
2024-07-02 17:49:46 +0800

    add unit test - get_bfc_zklogin_salt

---
+++ crates/sui-json-rpc-tests/tests/get_bfc_zklogin_salt_test.rs
@@ -0,0 +1,27 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::path::PathBuf;
+use sui_move_build::{BuildConfig, SuiPackageHooks};
+use sui_sdk::SuiClient;
+use sui_types::programmable_transaction_builder::ProgrammableTransactionBuilder;
+use sui_types::transaction::{TransactionData, TransactionKind};
+use test_cluster::TestClusterBuilder;
+
+#[tokio::test]
+async fn test_get_bfc_zklogin_salt() -> Result<(), anyhow::Error> {
+    let cluster = TestClusterBuilder::new().build().await;
+    let context = &cluster.wallet;
+
+    let client: SuiClient = context.get_client().await.unwrap();
+
+    let result = client
+        .read_api()
+        .get_bfc_zklogin_salt(String::from("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b"),
+                              String::from("000102030405060708090a0b0c"),
+                              String::from("f0f1f2f3f4f5f6f7f8f9"))
+        .await;
+    assert!(result.is_ok());
+
+    Ok(())
+}
