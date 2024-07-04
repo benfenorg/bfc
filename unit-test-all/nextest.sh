cb6a5556e0c25a2bd9a30cbd375dd464b6cc0b06
kakaxi <kakaxi002@hotmail.com>
2024-07-04 17:06:37 +0800

    update.

--- unit-test-all/nextest.sh
+++ unit-test-all/nextest.sh
@@ -2,5 +2,7 @@
 #cargo nextest run --profile ci  --retries 1
 #https://nexte.st/book/filter-expressions.html
 # --jobs 11 【limit working threads】
+#export TMPDIR=/path/to/custom/temp/dir
+#cargo nextest run -E 'not test(/sim_.*/)' --profile ci --retries 1 > cargo_logs 2>&1 &
 cargo nextest run -E 'not test(/sim_.*/)' --profile ci  --retries 1
 
