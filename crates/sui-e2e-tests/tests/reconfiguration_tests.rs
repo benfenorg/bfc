de0ede300bd411a84a1f083b4f50428caf49c548
wubin <18519247208@qq.com>
2024-08-09 10:36:44 +0800

    fix unit test

--- crates/sui-e2e-tests/tests/reconfiguration_tests.rs
+++ crates/sui-e2e-tests/tests/reconfiguration_tests.rs
@@ -920,7 +920,7 @@ async fn case_vote(http_client: &HttpClient, gas: &SuiObjectData, address: SuiAd
 #[sim_test]
 async fn sim_test_bfc_dao_revoke_vote() -> Result<(), anyhow::Error> {
     let cluster = TestClusterBuilder::new()
-        .with_epoch_duration_ms(90000)
+        .with_epoch_duration_ms(150000)
         .build().await;
     let http_client = cluster.rpc_client();
     let address = cluster.get_address_0();
@@ -1084,7 +1084,7 @@ async fn sim_test_destroy_terminated_proposal() -> Result<(), anyhow::Error> {
     let start_version = 23u64;
 
     let cluster = TestClusterBuilder::new()
-        .with_epoch_duration_ms(500000)
+        .with_epoch_duration_ms(4500000)
         .with_protocol_version(ProtocolVersion::new(start_version))
         .build()
         .await;
@@ -1162,7 +1162,7 @@ async fn sim_test_bfc_dao_queue_proposal_action() -> Result<(), anyhow::Error> {
     let start_version = 23u64;
 
     let cluster = TestClusterBuilder::new()
-        .with_epoch_duration_ms(500000)
+        .with_epoch_duration_ms(4500000)
         .with_protocol_version(ProtocolVersion::new(start_version))
         .build()
         .await;
@@ -1260,7 +1260,7 @@ async fn sim_test_bfc_dao_unvote_votingbfc() -> Result<(), anyhow::Error> {
 #[sim_test]
 async fn sim_test_bfc_dao_change_vote() -> Result<(), anyhow::Error> {
     let cluster = TestClusterBuilder::new()
-        .with_epoch_duration_ms(90000)
+        .with_epoch_duration_ms(150000)
         .build().await;
     let http_client = cluster.rpc_client();
     let address = cluster.get_address_0();
