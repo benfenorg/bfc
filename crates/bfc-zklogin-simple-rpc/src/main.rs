16a19a9fc48b0cba543c79533818e7e312ed5073
kakaxi <kakaxi002@hotmail.com>
2024-08-22 15:04:05 +0800

    fix

--- crates/bfc-zklogin-simple-rpc/src/main.rs
+++ crates/bfc-zklogin-simple-rpc/src/main.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 use axum::{routing::get, Router};
 use axum::routing::post;
-use std::net::SocketAddr;
+//use std::net::SocketAddr;
 use bfc_zklogin_simple_rpc::{verify_zk_signature, hello};
 use std::env;
 use std::process::exit;
@@ -20,11 +20,20 @@ async fn main() {
             post(move |request| verify_zk_signature(env_var, request))
         });
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 8003));
-    println!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(router.into_make_service())
-        .await
-        .unwrap();
+    //let addr = SocketAddr::from(([0, 0, 0, 0], 8003));
+
+    let addr = tokio::net::TcpListener::bind("0.0.0.0:8003").await.unwrap();
+    println!("listening on {:?}", addr);
+
+
+
+    axum::serve(addr, router).await.unwrap();
+
+
+    // axum::serve::bind(&addr)
+    //     .serve(router.into_make_service())
+    //     .await
+    //     .unwrap();
+
 }
 
