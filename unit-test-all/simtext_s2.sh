7fb2a4050be4646a100164f36b6502538f596ca5
kakaxi <kakaxi002@hotmail.com>
2024-08-29 12:03:03 +0800

    update. snapshot.

--- unit-test-all/simtext_s2.sh
+++ unit-test-all/simtext_s2.sh
@@ -1,3 +1,4 @@
+//need close watchdog, easy abort  with watchdog
 MSIM_DISABLE_WATCHDOG=1 cargo simtest sim_test_map_reducer
 MSIM_DISABLE_WATCHDOG=1 cargo simtest test_quorum_map_and_reduce_timeout
 MSIM_DISABLE_WATCHDOG=1 cargo simtest sim_test_quorum_driver_handling_overload_and_retry
