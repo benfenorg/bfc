827c6330b9fc2854134ed8eb619c85b4c163f561
francisL24 <fleet24564@gmail.com>
2024-09-02 14:47:41 +0800

    update fastcrypto version

--- crates/bfc-zklogin-simple-rpc/src/main.rs
+++ crates/bfc-zklogin-simple-rpc/src/main.rs
@@ -6,10 +6,15 @@ use std::net::SocketAddr;
 use bfc_zklogin_simple_rpc::{verify_zk_signature, hello};
 use std::env;
 use std::process::exit;
+use tracing::info;
+use tracing_subscriber::fmt;
 
 
 #[tokio::main]
 async fn main() {
+    let subscriber = fmt::Subscriber::new();
+    tracing::subscriber::set_global_default(subscriber).expect("Failed to set tracing subscriber");
+
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         eprintln!("Usage: bfc-zklogin-simple-rpc <env>(test,prod)");
@@ -23,7 +28,7 @@ async fn main() {
         });
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8003));
-    println!("listening on {}", addr);
+    info!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(router.into_make_service())
         .await
