56be1f5180c18aeaca4a15cbbc986b79de59b114
kakaxi <oday0311@hotmail.com>
2024-11-01 10:12:21 +0800

    update..change tempdb path to pathbuf temply for debug.

--- crates/sui-indexer/src/tempdb.rs
+++ crates/sui-indexer/src/tempdb.rs
@@ -11,6 +11,7 @@ use std::{
     time::{Duration, Instant},
 };
 use clap::arg;
+use tempfile::tempdir;
 use tracing::trace;
 use url::Url;
 
@@ -24,7 +25,8 @@ pub struct TempDb {
     //
     // NOTE: This needs to be the last entry in this struct so that the database is dropped before
     // and has a chance to gracefully shutdown before the directory is deleted.
-    dir: tempfile::TempDir,
+    //dir: tempfile::TempDir,
+    dir: PathBuf, // 修改为 PathBuf
 }
 
 impl TempDb {
@@ -33,10 +35,11 @@ impl TempDb {
     /// A fresh database will be initialized in a temporary directory that will be cleandup on drop.
     /// The running `postgres` service will be serving traffic on an available, os-assigned port.
     pub fn new() -> Result<Self> {
-        let dir = tempfile::TempDir::new()?;
+        //let dir = tempfile::TempDir::new()?;
+        let dir = tempdir()?.into_path(); // 获取 PathBuf
         let port = get_available_port();
 
-        let database = LocalDatabase::new_initdb(dir.path().to_owned(), port)?;
+        let database = LocalDatabase::new_initdb(dir.to_owned(), port)?;
 
         Ok(Self { dir, database })
     }
@@ -50,7 +53,7 @@ impl TempDb {
     }
 
     pub fn dir(&self) -> &Path {
-        self.dir.path()
+        self.dir.as_path()
     }
 }
 
