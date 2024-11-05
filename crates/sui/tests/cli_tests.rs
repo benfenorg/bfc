60d65a64f57c9ce9013de5e9021fc1d990a9584a
kakaxi <kakaxi002@hotmail.com>
2024-11-05 10:01:30 +0800

    update

--- crates/sui/tests/cli_tests.rs
+++ crates/sui/tests/cli_tests.rs
@@ -464,6 +464,13 @@ async fn sim_test_object_info_get_command() -> Result<(), anyhow::Error> {
 }
 
 #[sim_test]
+async fn sim_test_start_cluster() -> Result<(), anyhow::Error> {
+
+    let mut test_cluster = TestClusterBuilder::new().build().await;
+
+    Ok(())
+}
+#[sim_test]
 async fn sim_test_gas_command() -> Result<(), anyhow::Error> {
     let mut test_cluster = TestClusterBuilder::new().build().await;
     let rgp = test_cluster.get_reference_gas_price().await;
