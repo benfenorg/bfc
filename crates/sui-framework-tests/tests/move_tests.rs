3dddb0d2375d5cf862379ba130e31d5349265bed
francisL24 <fleet24564@gmail.com>
2024-12-04 17:48:28 +0800

    fix: stdlib gas limit

--- crates/sui-framework-tests/tests/move_tests.rs
+++ crates/sui-framework-tests/tests/move_tests.rs
@@ -53,7 +53,7 @@ pub(crate) fn tests(path: &Path) -> datatest_stable::Result<()> {
     config.config.lint_flag = LintFlag::LEVEL_DEFAULT;
 
     let move_config = config.config.clone();
-    let mut testing_config = UnitTestingConfig::default_with_bound(Some(3_000_000_000));
+    let mut testing_config = UnitTestingConfig::default_with_bound(Some(1_000_000_0));
     testing_config.filter = std::env::var("FILTER").ok().map(|s| s.to_string());
 
     assert_eq!(
