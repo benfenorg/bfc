d75f8e41544f0e3d86196d486f662342d0ff3b41
kakaxi <kakaxi002@hotmail.com>
2025-01-02 11:08:03 +0800

    change tempdb for test.

--- crates/sui-indexer/src/tempdb.rs
+++ crates/sui-indexer/src/tempdb.rs
@@ -10,6 +10,7 @@ use std::{
     process::{Child, Command},
     time::{Duration, Instant},
 };
+use tempfile::tempdir;
 //use clap::arg;
 //use tempfile::tempdir;
 use tracing::trace;
@@ -25,8 +26,8 @@ pub struct TempDb {
     //
     // NOTE: This needs to be the last entry in this struct so that the database is dropped before
     // and has a chance to gracefully shutdown before the directory is deleted.
-    dir: tempfile::TempDir,
-    //dir: PathBuf, // 修改为 PathBuf
+    //dir: tempfile::TempDir,
+    dir: PathBuf, // 修改为 PathBuf
 }
 
 impl TempDb {
@@ -35,11 +36,11 @@ impl TempDb {
     /// A fresh database will be initialized in a temporary directory that will be cleandup on drop.
     /// The running `postgres` service will be serving traffic on an available, os-assigned port.
     pub fn new() -> Result<Self> {
-        let dir = tempfile::TempDir::new()?;
-        //let dir = tempdir()?.into_path(); // 获取 PathBuf
+        //let dir = tempfile::TempDir::new()?;
+        let dir = tempdir()?.into_path(); // 获取 PathBuf
         let port = get_available_port();
 
-        let database = LocalDatabase::new_initdb(dir.path().to_owned(), port)?;
+        let database = LocalDatabase::new_initdb(dir.clone(), port)?;
 
         Ok(Self { dir, database })
     }
@@ -53,7 +54,7 @@ impl TempDb {
     }
 
     pub fn dir(&self) -> &Path {
-        self.dir.path()
+        &self.dir
     }
 }
 
