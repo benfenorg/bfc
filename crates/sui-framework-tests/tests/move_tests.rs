bae03489dde0f47cdc2f3fdfb3fb6b31703278b9
david20240515 <david20240515@gmail.com>
2025-01-13 09:41:21 +0800

    modify test gas limit

--- crates/sui-framework-tests/tests/move_tests.rs
+++ crates/sui-framework-tests/tests/move_tests.rs
@@ -56,6 +56,7 @@ pub(crate) fn tests(path: &Path) -> datatest_stable::Result<()> {
     // TODO: Remove this when we support per-test gas limits.
     let mut testing_config = UnitTestingConfig::default_with_bound(Some(3_000_000));
     let mut testing_config = UnitTestingConfig::default_with_bound(Some(1_000_000_0));
+    let mut testing_config = UnitTestingConfig::default_with_bound(Some(1_000_000_00));
     testing_config.filter = std::env::var("FILTER").ok().map(|s| s.to_string());
 
     assert_eq!(
