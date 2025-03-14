b16b2ebd4603ac526baf5229b69840737590ac4a
kakaxi <kakaxi002@hotmail.com>
2025-03-14 10:48:28 +0800

    update

--- unit-test-all/nextest.sh
+++ unit-test-all/nextest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#cargo nextest run --profile ci  --retries 1
+#UPDATE_BASELINE=1 cargo nextest run --profile ci  --retries 1
 #https://nexte.st/book/filter-expressions.html
 # --jobs 11 【limit working threads】
 #export TMPDIR=/path/to/custom/temp/dir
