a8bfb0ea0ca4fb9dd21201d12311a76772314859
lifeimm100 <172345037+lifeimm100@users.noreply.github.com>
2025-03-27 14:09:41 +0800

    fix(server): add missing leading slash to EXTERNAL_TO_SUI_TX_PATH
    Ensure consistency in path definitions by adding a leading slash to EXTERNAL_TO_SUI_TX_PATH, matching the format of other path constants

--- crates/sui-bridge/src/server/mod.rs
+++ crates/sui-bridge/src/server/mod.rs
@@ -49,7 +49,7 @@ pub const METRICS_KEY_PATH: &str = "/metrics_pub_key";
 pub const ETH_TO_SUI_TX_PATH: &str = "/sign/bridge_tx/eth/sui/:tx_hash/:event_index";
 pub const SUI_TO_ETH_TX_PATH: &str = "/sign/bridge_tx/sui/eth/:tx_digest/:event_index";
 pub const SUI_TO_ETH_SEND_BACK_TX_PATH: &str = "/sign/bridge_tx/sui/eth/send/back/:tx_digest/:event_index";
-pub const EXTERNAL_TO_SUI_TX_PATH: &str = "sign/bridge_tx/external/sui/:tx_digest/:event_index";
+pub const EXTERNAL_TO_SUI_TX_PATH: &str = "/sign/bridge_tx/external/sui/:tx_digest/:event_index";
 pub const COMMITTEE_BLOCKLIST_UPDATE_PATH: &str =
     "/sign/update_committee_blocklist/:chain_id/:nonce/:type/:keys";
 pub const EMERGENCY_BUTTON_PATH: &str = "/sign/emergency_button/:chain_id/:nonce/:type";
