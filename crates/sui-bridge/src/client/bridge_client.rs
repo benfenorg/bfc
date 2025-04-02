1bb79eed70463b4a13a69a275c9e3fd3d025210d
lizhengda <zero17280124@gmail.com>
2025-04-02 17:22:17 +0800

    fix

--- crates/sui-bridge/src/client/bridge_client.rs
+++ crates/sui-bridge/src/client/bridge_client.rs
@@ -151,7 +151,7 @@ impl BridgeClient {
                 let witness_address=format!("{:x}",a.witness_address.clone());
 
                 format!(
-                    "sign/add_external_coin_witness/{chain_id}/{nonce}/{coin_type}/{eth_address}"
+                    "sign/add_external_coin_witness/{chain_id}/{nonce}/{coin_type}/{witness_address}"
                 )
             }
             BridgeAction::RemoveExternalCoinWitnessAction(a) => {
@@ -160,7 +160,7 @@ impl BridgeClient {
                 let coin_type = a.coin_type.clone();
                 let witness_address=format!("{:x}",a.witness_address.clone());
                 format!(
-                    "sign/add_external_coin_witness/{chain_id}/{nonce}/{coin_type}/{eth_address}"
+                    "sign/add_external_coin_witness/{chain_id}/{nonce}/{coin_type}/{witness_address}"
                 )
             }
             BridgeAction::AddExternalCoinTargetAction(a) => {
