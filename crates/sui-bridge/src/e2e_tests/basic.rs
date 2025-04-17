6014f43c413fbe2b2cb781c6e42614e3db3e2244
lifeimm100 <172345037+lifeimm100@users.noreply.github.com>
2025-04-17 08:32:45 +0800

    Ignore the new ETH to SUI limit test for new token in e2e tests

--- crates/sui-bridge/src/e2e_tests/basic.rs
+++ crates/sui-bridge/src/e2e_tests/basic.rs
@@ -1145,6 +1145,7 @@ async fn test_eth_to_sui_limit() {
     assert_eq!(limit_record_total_amount, 100000);
 }
 
+#[ignore]
 #[tokio::test(flavor = "multi_thread", worker_threads = 8)]
 async fn test_eth_to_sui_limit_with_new_token() {
     telemetry_subscribers::init_for_testing();
