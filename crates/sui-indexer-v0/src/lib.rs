fa5a429371cd394d3190d3ebfeaf71e627245f4b
david20240515 <david20240515@gmail.com>
2025-04-17 14:03:53 +0800

    modify pg config

--- crates/sui-indexer-v0/src/lib.rs
+++ crates/sui-indexer-v0/src/lib.rs
@@ -414,8 +414,8 @@ struct PgConectionPoolConfig {
 
 impl PgConectionPoolConfig {
     const DEFAULT_POOL_SIZE: u32 = 10;
-    const DEFAULT_CONNECTION_TIMEOUT: Duration = Duration::from_secs(30);
-    const DEFAULT_STATEMENT_TIMEOUT: Duration = Duration::from_secs(60);
+    const DEFAULT_CONNECTION_TIMEOUT: Duration = Duration::from_secs(150);
+    const DEFAULT_STATEMENT_TIMEOUT: Duration = Duration::from_secs(300);
 
     fn connection_config(&self) -> PgConnectionConfig {
         PgConnectionConfig {
