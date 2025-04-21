eda2a1e3cff1fdff8b3f9fb0e0ec05776aa03fde
lizhengda <zero17280124@gmail.com>
2025-04-21 12:42:42 +0800

    update

--- bridge/evm/contracts/SuiBridge.sol
+++ bridge/evm/contracts/SuiBridge.sol
@@ -154,9 +154,8 @@ contract SuiBridge is ISuiBridge, CommitteeUpgradeable, PausableUpgradeable {
             IERC20(tokenAddress).allowance(msg.sender, address(this)) >= amount,
             "SuiBridge: Insufficient allowance"
         );
-        if (tokenID==BridgeUtils.USDC || tokenID ==BridgeUtils.USDT){
-
-            require(amount<limiter.getUsdMaxLimit(),"SuiBridge: USD Exceed Limit");
+        if (tokenID == BridgeUtils.USDC || tokenID == BridgeUtils.USDT) {
+            require(amount < limiter.getUsdMaxLimit(), "SuiBridge: USD Exceed Limit");
         }
 
         // calculate old vault balance
