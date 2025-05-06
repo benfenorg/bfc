5e26320577dd65c34a5e569d9a4cb0b2aa30dfdb
kakaxi <kakaxi002@hotmail.com>
2025-05-06 16:29:54 +0800

    update.

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -3136,7 +3136,7 @@ impl ProtocolConfig {
                 }
 
                 71 => {
-                    cfg.feature_flags.anonymous_coin_open = false;
+                    cfg.feature_flags.anonymous_coin_open = true;
                 }
                 // Use this template when making changes:
                 //
