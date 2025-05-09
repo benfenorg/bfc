790239187f5a7166f333d066da7ae5d19cbb96ff
lizhengda <zero17280124@gmail.com>
2025-05-09 18:10:34 +0800

    add deploy bnb usd token

--- bridge/evm/script/deploy_bridge.s.sol
+++ bridge/evm/script/deploy_bridge.s.sol
@@ -59,9 +59,17 @@ contract DeployBridge is Script {
             deployConfig.weth = address(new WETH());
 
             // deploy mock tokens
+            IERC20 USDC;
+            IERC20 USDT;
+
+            if (chainIDHash == keccak256(abi.encode("31337"))){
+                USDC = new MockUSDC();
+                USDT = new MockUSDT();
+            }else{
+                USDC = new MockBNBUSDC();
+                USDT = new MockBNBUSDT();
+            }
             MockWBTC wBTC = new MockWBTC();
-            MockUSDC USDC = new MockUSDC();
-            MockUSDT USDT = new MockUSDT();
             MockKA KA = new MockKA();
             MockBUSD BUSD = new MockBUSD();
             MockBNB BNB = new MockBNB();
@@ -77,7 +85,7 @@ contract DeployBridge is Script {
             deployConfig.supportedTokens[4] = address(USDT);
             deployConfig.supportedTokens[5] = address(BUSD);
             deployConfig.supportedTokens[6] = address(BNB);
-            
+
             deployConfig.tokenIds = new uint256[](7);
             deployConfig.tokenIds[0] = 0;
             deployConfig.tokenIds[1] = 1;
@@ -100,7 +108,7 @@ contract DeployBridge is Script {
                 deployConfig.suiDecimals[3] = 9;
                 deployConfig.suiDecimals[4] = 9;
             }
-            
+
             deployConfig.suiDecimals[5] = 9;
             deployConfig.suiDecimals[6] = 8;
         }
