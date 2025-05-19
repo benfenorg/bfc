0eb0deeda83d4d3948d7d3731280eb294c7e9d62
zerokk246 <zero.kk246@gmail.com>
2025-05-19 18:55:06 +0800

    fix: hard code to fix bad btc query data

--- crates/sui-bridge/src/events.rs
+++ crates/sui-bridge/src/events.rs
@@ -652,6 +652,7 @@ pub mod tests {
             target_address: sanitized_event.eth_address.as_bytes().to_vec(),
             token_type: sanitized_event.token_id,
             amount_sui_adjusted: sanitized_event.amount_sui_adjusted,
+            benfen_amount: 0,
         };
 
         let tx_digest = TransactionDigest::random();
@@ -814,6 +815,7 @@ pub mod tests {
             target_address: EthAddress::random().as_bytes().to_vec(),
             token_type: TOKEN_ID_SUI,
             amount_sui_adjusted: 0,
+            benfen_amount: 0,
         };
         match EmittedSuiToEthTokenBridgeV1::try_from(emitted_event).unwrap_err() {
             BridgeError::ZeroValueBridgeTransfer(_) => (),
--- crates/sui-bridge/src/sui_client.rs
+++ crates/sui-bridge/src/sui_client.rs
@@ -1062,6 +1062,7 @@ mod tests {
             target_address: sanitized_event_1.eth_address.as_bytes().to_vec(),
             token_type: sanitized_event_1.token_id,
             amount_sui_adjusted: sanitized_event_1.amount_sui_adjusted,
+            benfen_amount: 0,
         };
 
         let mut sui_event_1 = SuiEvent::random_for_testing();
