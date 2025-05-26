823803e1daebfdad533e01094b1b9cef7d700706
lifeimm100 <172345037+lifeimm100@users.noreply.github.com>
2025-05-26 10:40:51 +0800

    Refactor AML check logic in eth to Sui bridge action to conditionally skip based on event index

--- crates/sui-bridge/src/aml_checker.rs
+++ crates/sui-bridge/src/aml_checker.rs
@@ -96,16 +96,19 @@ P: SuiClientInner + 'static,{
                 BridgeAction::EthToSuiBridgeAction(action_inner) => {
                     let eth_address = action_inner.eth_bridge_event.eth_address;
                     let tx_hash = action_inner.eth_tx_hash.as_bytes().to_vec();
-                    let is_passed = true;
-                    // TODO: skip aml check for now
-                    // check_aml_risk_score(
-                    //     action_inner.eth_bridge_event.eth_chain_id,
-                    //     action_inner.eth_bridge_event.token_id,
-                    //     eth_address,
-                    //     aml_key.clone()
-                    // ).await;
+                    let skip_aml_check = if action_inner.eth_event_index > u8::MAX as u16 {
+                        true
+                    } else {
+                        false
+                    };
+                    let is_passed = check_aml_risk_score(
+                        action_inner.eth_bridge_event.eth_chain_id,
+                        action_inner.eth_bridge_event.token_id,
+                        eth_address,
+                        aml_key.clone()
+                    ).await;
                     info!("aml checker eth address:{:?} is_passed: {:?} tx_hash: {:?}", &eth_address, &is_passed, &tx_hash);
-                    if is_passed {
+                    if skip_aml_check || is_passed {
                         store.insert_pending_actions(&[bridge_action.clone()]).unwrap_or_else(|e| {
                             panic!("Write to DB should not fail: {:?}", e);
                         });
