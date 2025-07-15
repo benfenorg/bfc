fcd2ac4d49d591689f837224aa54f5c96c28cdb7
kakaxi <oday0311@hotmail.com>
2025-07-15 14:54:54 +0800

    update..

--- crates/sui-indexer-v0/src/apis/coin_api.rs
+++ crates/sui-indexer-v0/src/apis/coin_api.rs
@@ -96,7 +96,13 @@ impl CoinReadApiServer for CoinReadApi {
     }
 
     async fn get_anonyment_coin_metadata(&self, coin_type: String) -> RpcResult<Option<SuiCoinMetadata>> {
-        self.fullnode.get_anonyment_coin_metadata(coin_type).await
+        self.fullnode.get_anonyment_coin_metadata(coin_type).await.map_err(|e| {
+            ErrorObject::owned(
+                ErrorCode::InternalError.code(),
+                format!("Client error: {}", e),
+                None::<()>,
+            )
+        })
     }
 
     async fn get_total_supply(&self, coin_type: String) -> RpcResult<Supply> {
