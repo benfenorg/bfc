2d235f31e6a98161fc0dfa9c6efdc02c3fff70fa
helloboy1234 <geb789@163.com>
2025-10-10 16:59:55 +0800

    fix unit test

--- crates/bfc-anonymous-server/src/client_test.rs
+++ crates/bfc-anonymous-server/src/client_test.rs
@@ -4,6 +4,7 @@ use std::error::Error;
 use std::str::FromStr;
 use log::info;
 use sui_types::base_types::SuiAddress;
+use crate::AnonymousRestoreElementParams;
 
 #[derive(Debug)]
 #[allow(unused)]
@@ -189,21 +190,25 @@ impl AnonymousClient {
         objectid: String,
         publickey: Vec<u8>,
     ) -> TestResult {
-        let params = json!([{
-            "value1": value1,
-            "value2": value2,
-            "signature": signature,
-            "objectid": objectid,
-            "publickey": publickey,
-        },
-            {
-            "value1": value1,
-            "value2": value2,
-            "signature": signature,
-            "objectid": objectid,
-            "publickey": publickey,
-        }
-        ]);
+
+        let params = Value::Object(
+            serde_json::Map::from_iter([
+                ("publickey".to_string(), json!(publickey)),
+                ("signature".to_string(), json!(signature)),
+                ("anonymous_restore_array".to_string(), Value::Array(vec![
+                    Value::Object(serde_json::Map::from_iter([
+                        ("value1".to_string(), json!(value1)),
+                        ("value2".to_string(), json!(value2)),
+                        ("objectid".to_string(), serde_json::Value::String(objectid.clone())),
+                    ])),
+                    Value::Object(serde_json::Map::from_iter([
+                        ("value1".to_string(), json!(value1)),
+                        ("value2".to_string(), json!(value2)),
+                        ("objectid".to_string(), serde_json::Value::String(objectid.clone())),
+                    ])),
+                ])),
+            ])
+        );
 
         match self
             .send_rpc_request("bfcx_getAnonymousRestoreValueArray", params, 4)
@@ -325,7 +330,7 @@ impl AnonymousClient {
     }
     async fn test_recover_array_with_signature(&self, share1: String, share2: String) -> Result<Vec<u64>, serde_json::error::Error> {
         // test restore 20
-        let signature = "0f31177f8ece16b2cfb8c1ba0b71f73252acaa6cfbbe13d36c3320617f05bc7f9a860f16c8b10c787455a01ca7bcca3469858aae4e369bc994ab64967f1fd20f";
+        let signature = "724f9ae6ae54c249d77b9c9589a289a4ffb74358f888624602f89baf2e779460faefe09cb70bdf5e6730f16b7317893baa5b99b1d811842237e60103c4106f04";
         let publickey = "8496d3d932986b43bb64b5d5c7548d5c97a73aebf4301447f3746680b2114ae1";
 
         let object_id = SuiAddress::from_str(
