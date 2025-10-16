30c355e5f463b351d0df866b3dfd7511d6655e02
lifeimm100 <172345037+lifeimm100@users.noreply.github.com>
2025-10-16 09:36:41 +0800

    Add get_parsed_defi_transfer_out_message method to SuiMockClient
    This commit introduces the `get_parsed_defi_transfer_out_message` method in the SuiMockClient, enhancing the mock client interface to support parsing of Defi transfer out messages. This addition aligns with the ongoing improvements to the Sui Bridge's functionality for handling Defi transactions.

--- crates/sui-bridge/src/sui_mock_client.rs
+++ crates/sui-bridge/src/sui_mock_client.rs
@@ -13,7 +13,7 @@ use sui_json_rpc_types::{ SuiTransactionBlockResponse};
 use sui_json_rpc_types::{EventFilter, EventPage, SuiEvent};
 use sui_types::base_types::{ObjectID, SuiAddress};
 use sui_types::base_types::ObjectRef;
-use sui_types::bridge::{BridgeCommitteeSummary, BridgeSummary, BridgeTokenMetadata, BridgeTreasurySummary, MoveTypeParsedTokenTransferMessageV2};
+use sui_types::bridge::{BridgeCommitteeSummary, BridgeSummary, BridgeTokenMetadata, BridgeTreasurySummary, MoveTypeParsedDefiTransferOutMessage, MoveTypeParsedTokenTransferMessageV2};
 use sui_types::digests::TransactionDigest;
 use sui_types::event::EventID;
 use sui_types::gas_coin::GasCoin;
@@ -386,6 +386,15 @@ impl SuiClientInner for SuiMockClient {
         _source_chain_id: u8,
         _seq_number: u64,
     ) -> Result<Option<MoveTypeParsedTokenTransferMessageV2>, BridgeError> {
+        unimplemented!()
+    }
+
+    async fn get_parsed_defi_transfer_out_message(
+        &self,
+        _bridge_object_arg: ObjectArg,
+        _source_chain_id: u8,
+        _seq_number: u64,
+    ) -> Result<Option<MoveTypeParsedDefiTransferOutMessage>, BridgeError> {
         unimplemented!()
     }
 
