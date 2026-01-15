253a0aa578044043293dcdfd413bbea2e356a52f
lifeimm100 <172345037+lifeimm100@users.noreply.github.com>
2026-01-15 15:18:47 +0800

    Update protocol version to 79 and adjust related snapshots
    - Incremented the maximum supported protocol version from 78 to 79 in `lib.rs`.
    - Updated protocol version in various snapshot test files to reflect the new version.
    - Added new package IDs in the `manifest.json` for version 79.

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -18,7 +18,7 @@ use tracing::{info, warn};
 
 /// The minimum and maximum protocol versions supported by this build.
 const MIN_PROTOCOL_VERSION: u64 = 1;
-const MAX_PROTOCOL_VERSION: u64 = 78;
+const MAX_PROTOCOL_VERSION: u64 = 79;
 
 // Record history of protocol version allocations here:
 //
@@ -3288,10 +3288,8 @@ impl ProtocolConfig {
                 }
                 78 => {
                 }
-
-
-
-
+                79 => {
+                }
                 // Use this template when making changes:
                 //
                 //     // modify an existing constant.
