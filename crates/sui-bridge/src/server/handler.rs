8df68047b90ebf62df71b4ac6b984c1effa631d1
lizhengda <zero17280124@gmail.com>
2026-02-04 11:54:13 +0800

    update

--- crates/sui-bridge/src/server/handler.rs
+++ crates/sui-bridge/src/server/handler.rs
@@ -246,6 +246,7 @@ where
             let tx_hash = &external_action.sui_bridge_event.tx_hash;
             let amount = external_action.sui_bridge_event.amount;
             let chain_id = external_action.sui_bridge_event.source_chain;
+            let token_id = external_action.sui_bridge_event.token_id;
 
             // check target address in whitelist
             let summary = self.sui_client.get_bridge_summary().await;
@@ -270,7 +271,12 @@ where
                 BridgeChainId::TronMainnet | BridgeChainId::TronTestnet => {
                     // check tron txn: only support TRC20
                     // readme: amount is benfen amount, not tron amount, so we need to convert it
-                    let tron_amount = amount / 1_000;
+                    let mut tron_amount = amount / 1_000;
+                    if  token_id==3 || token_id==4{
+                        tron_amount = amount ;
+                    };
+
+                   
                     let external_rpc = self.external_rpc.as_ref().ok_or_else(|| {
                         BridgeError::Generic("External RPC config not found".to_string())
                     })?;
@@ -283,7 +289,11 @@ where
                     // check solana txn: only support USDC/USDT
 
                     // readme: amount is benfen amount, not solana amount, so we need to convert it
-                    let sol_amount = amount / 1_000;
+                    let mut sol_amount = amount / 1_000;
+                    if  token_id==3 || token_id==4{
+                        sol_amount = amount;
+                    };
+
                     let external_rpc = self.external_rpc.as_ref().ok_or_else(|| {
                         BridgeError::Generic("External RPC config not found".to_string())
                     })?;
