de742624bb4395b59f3dd417a21491dab1082d65
Zero One <zero.kk246@gmail.com>
2026-02-06 15:11:04 +0800

    Increment protocol version

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -18,7 +18,7 @@ use tracing::{info, warn};
 
 /// The minimum and maximum protocol versions supported by this build.
 const MIN_PROTOCOL_VERSION: u64 = 1;
-const MAX_PROTOCOL_VERSION: u64 = 81;
+const MAX_PROTOCOL_VERSION: u64 = 82;
 
 // Record history of protocol version allocations here:
 //
@@ -3293,6 +3293,8 @@ impl ProtocolConfig {
                 80 => {
                 }
                 81 => {
+                }
+                82 => {
                 }
                 // Use this template when making changes:
                 //
