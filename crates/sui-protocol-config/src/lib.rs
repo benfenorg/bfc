61a32e3b9489ea928b9e4a91a84f3623c5373831
Zero One <zero.kk246@gmail.com>
2026-03-09 20:51:19 +0800

    revert test version

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -18,7 +18,7 @@ use tracing::{info, warn};
 
 /// The minimum and maximum protocol versions supported by this build.
 const MIN_PROTOCOL_VERSION: u64 = 1;
-const MAX_PROTOCOL_VERSION: u64 = 83;
+const MAX_PROTOCOL_VERSION: u64 = 79;
 
 
 // Record history of protocol version allocations here:
@@ -3290,14 +3290,6 @@ impl ProtocolConfig {
                 78 => {
                 }
                 79 => {
-                }
-                80 => {
-                }
-                81 => {
-                }
-                82 => {
-                }
-                83 => {
                 }
 
                 // Use this template when making changes:
